5ca65cb3ba52459bbf8a85bd1b305970cf38206a
Kerry Back <kerryback@gmail.com>
2023-10-10 14:03:33 -0500

    new slides

--- slides.py
+++ slides.py
@@ -2,6 +2,6 @@ import os
 file = input("\nNotebook name: ")
 os.system(f'jupyter nbconvert notebooks\{file}.ipynb --to slides --SlidesExporter.reveal_scroll=True')
 os.system(f"move notebooks\{file}.slides.html docs\{file}.slides.html")
-os.system(f"git add .")
+os.system("git add .")
 os.system('git commit -m "new slides"')
-os.system(f"git push origin main")
+os.system("git push origin main")
