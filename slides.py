8dcc99164160f1f52052d845512f521b8c12f9ed
kerryback <74005663+kerryback@users.noreply.github.com>
2023-10-10 20:42:19 -0500

    new slides

--- slides.py
+++ slides.py
@@ -1,7 +1,9 @@
 import os
 file = input("\nNotebook name: ")
 os.system(f'jupyter nbconvert notebooks\{file}.ipynb --to slides --SlidesExporter.reveal_scroll=True')
+os.system(f'decktape automatic notebooks\{file}.slides.html notebooks\{file}.slides.pdf')
 os.system(f"move notebooks\{file}.slides.html docs\{file}.slides.html")
+os.system(f"move notebooks\{file}.slides.pdf pdfs\{file}.slides.html")
 os.system("git add .")
 os.system('git commit -m "new slides"')
 os.system("git push origin main")
